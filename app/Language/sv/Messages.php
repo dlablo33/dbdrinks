bf2f948aedce38fb260f30d182a7806b2b9a183e
dlablo33 <cesar.saucedolz@uanl.edu.mx>
2024-10-10 15:19:53 -0600

    Inicio de dbdrinks y base del proyecto

---
+++ app/Language/sv/Messages.php
@@ -0,0 +1,15 @@
+<?php
+return [
+	"first_name" => "Förnamn",
+	"last_name" => "Efternamn",
+	"message" => "Meddelande",
+	"message_placeholder" => "Ditt meddelande här...",
+	"message_required" => "Meddelande krävs",
+	"multiple_phones" => "(vid flera mottagare, avdela mobilnummer med komma)",
+	"phone" => "Telefonnummer",
+	"phone_number_required" => "Telefonnummer krävs",
+	"phone_placeholder" => "Mobiltelefonnummer...",
+	"sms_send" => "Skicka SMS",
+	"successfully_sent" => "Meddelandet skickat till: ",
+	"unsuccessfully_sent" => "Meddelandet skickades ej till: ",
+];
